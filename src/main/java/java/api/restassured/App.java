96d9196220c2e919510cb1086db09ccc5082e12b
Archana <Archana@LAPTOP-F2Q7UN0G>
2022-06-20 00:48:51 +0530

    first commit

---
+++ src/main/java/java/api/restassured/App.java
@@ -0,0 +1,17 @@
+package java.api.restassured;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        RestAssured.baseURI="https://api.publicapis.org/entries";
+        //given()
+    }
+}
